f472b0fe717362c57e01f56fdd5ed869ae76f567
yjlim0428 <yjlim9898@gmail.com>
2023-02-15 19:34:47 +0900

    정렬

--- 정렬/H-Index.py
+++ 정렬/H-Index.py
@@ -1,10 +1,19 @@
+# def solution(citations):
+#     citations.sort(reverse=True)
+#     for i in range(len(citations)):
+#         if len(citations[0:i + 1]) >= i+ 1 and i + 1  >= len(citations[i:len(citations)]):
+#             return citations[i]
+
+
 def solution(citations):
-    citations.sort(reverse=True)
-    print("citations:",citations)
-    for i in range(len(citations)):
-        print(citations[0:i + 1], citations[i:len(citations)])
-        if len(citations[0:i + 1]) >= i+ 1 and i + 1  >= len(citations[i:len(citations)]):
-            return citations[i]
+    answer = 0
+    citations = sorted(citations) # 오름차순 정렬
+    for idx, paper in enumerate(citations):
+        print(idx,paper)
+        if paper >= len(citations[idx:]):
+            answer = len(citations[idx:])
+            break
 
+    return answer
 
-print(solution([7]))
+print(solution([3, 0, 6, 1, 5]))
